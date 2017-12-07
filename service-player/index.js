a75cf58430e8472a514a4e94ff6786c51568de50
Mike Reinstein <reinstein.mike@gmail.com>
2017-12-06 23:38:53 -0800

    implemented basic player

---
+++ service-player/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const delay = require('delay')
+const exec  = require('child_process').execSync
+const fs    = require('fs')
+
+
+const cacheDir = __dirname + '/../service-content/cache'
+
+
+async function run() {
+  if(!fs.existsSync(cacheDir))
+    fs.mkdirSync(cacheDir)
+
+  while(true) {
+    if(fs.existsSync(cacheDir)) {
+      const files = fs.readdirSync(cacheDir)
+      console.log(files)
+
+      // play a random file
+      let idx = Math.floor(Math.random() * files.length)
+      exec(`omxplayer ${cacheDir}/${files[idx]} --blank`)
+    }
+
+    await delay(30000) // wait 30 seconds
+  }
+}
+
+run()
