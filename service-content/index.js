60a8b07874e86edeb04389043b8b4315a79ef927
Mike Reinstein <reinstein.mike@gmail.com>
2017-12-06 23:26:08 -0800

    implemented video downloader

--- service-content/index.js
+++ service-content/index.js
@@ -1,15 +1,41 @@
 'use strict'
 
 const delay = require('delay')
+const exec  = require('child_process').execSync
 const fetch = require('node-fetch')
+const fs    = require('fs')
+
+
+const cacheDir = __dirname + '/cache'
+
+
+async function getCacheEntries() {
+  let response = await fetch('https://s3.amazonaws.com/smartdecovideos/smartdecoPlaylist.txt')
+  let text = await response.text()
+  let entries = text.split('\n')
+  return entries
+}
+
+
+async function cacheNewEntries(entries) {
+  for(let i=0; i < entries.length; i++) {
+    let entry = entries[i]
+    if(!fs.existsSync(cacheDir + '/' + entry)) {
+      let url = `https://s3.amazonaws.com/smartdecovideos/${entry}`
+      exec(`wget ${url} -O ${cacheDir}/${entry}`)
+    }
+  }
+}
 
 
 async function run() {
+  if(!fs.existsSync(cacheDir))
+    fs.mkdirSync(cacheDir)
+
   while(true) {
-    let response = await fetch('https://s3.amazonaws.com/smartdecovideos/smartdecoPlaylist.txt')
-    let text = await response.text()
-    console.log('text:', text)
-    await delay(10000)
+    const entries = await getCacheEntries()
+    cacheNewEntries(entries)
+    await delay(3600 * 1000) // check every hour for new content
   }
 }
 
